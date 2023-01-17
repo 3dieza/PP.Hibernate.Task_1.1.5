6577e76fdb5f4de5c5ab564e483b1e5075748364
Nizamov Timur <3dieza@gmail.com>
2023-01-17 10:35:06 +0300

    in class User override toString

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,28 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/?user=root";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "rootroot";
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            System.out.println("Connection OK");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection OK");
+        }
+        return connection;
+    }
+
+
 }
